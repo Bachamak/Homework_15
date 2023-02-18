4298248ffa2d844e79bc7da2492bd6671d1f17ff
Bachamak <tarasenkodaniilromanovich@gmail.com>
2023-02-19 00:57:32 +0200

    Исправленное

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let b = prompt('Напишите второе число');
 
 function min(a, b) {
     return a < b ? a : b;
-}
+};
 console.log(min(a, b));
 
 // Напишіть функцію pow(x, n), яка повертає число x в n степені.
@@ -17,3 +17,11 @@ function pow(x, n) {
     return x >= 1 && n >= 1 ? x ** n : alert('Пожалуйста, введите числа больше нуля');
 };
 console.log(pow(x, n));
+
+// Условие возраста
+
+function func() {
+    let elem = prompt('Напишите ваш возраст для допуска на сайт');
+    return elem < 19 || elem > 65 ? alert('Тебе нет 18 лет, подожди еще немного!') : alert('Добро пожаловать!');
+};
+console.log(func());
